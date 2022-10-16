237f5518fcb088a5742556a6e4a723fc81fe5643
Pranavbj <pranavbharadwaj002@gmail.com>
2022-10-16 23:29:20 +0530

    cards

--- database_connection.php
+++ database_connection.php
@@ -2,8 +2,9 @@
 
 //database_connection.php
 
-$connect = new PDO("mysql:host=localhost; dbname=cattle_inventory_system", "root", "Prabhat@12");
+$connect = new PDO("mysql:host=localhost; dbname=cattle_inventory_system", "root", "Pranav@123");
 
 session_start();
 
 ?>
+
