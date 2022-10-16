ce4103f3da2013367031e2bfa70b33bb5abf87ad
prabhatsuman <mr.prabhatsuman@gmail.com>
2022-10-16 20:44:04 +0530

    1

--- database_connection.php
+++ database_connection.php
@@ -2,7 +2,7 @@
 
 //database_connection.php
 
-$connect = new PDO("mysql:host=localhost; dbname=cattle_inventory_system", "root", "Pranav@123");
+$connect = new PDO("mysql:host=localhost; dbname=cattle_inventory_system", "root", "Prabhat@12");
 
 session_start();
 
