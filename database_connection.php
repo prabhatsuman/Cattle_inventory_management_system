8a1f6f9458396fbb24f70cff4d08a984a0ef0a64
prabhatsuman <mr.prabhatsuman@gmail.com>
2024-05-24 01:09:54 +0530

    changed database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -1,5 +1,4 @@
 <?php
-//database_connection.php
-$connect = new PDO("mysql:host=dbms_project-db-1; dbname=cattle_inventory_system", "root", "Prabhat@12");
+$connect = new PDO("mysql:host=cattle_inventory_management_system-db-1; dbname=cattle_inventory_system", "root", "Prabhat@12");
 session_start();
 ?>
